1e3b052f55cf508bceaf05f75d6f94eb8b3a019d
aga-mn <molda.nieweglowska@gmail.com>
2014-07-28 10:27:11 +0200

    tutorial 7 creating a WS

--- src/testmart/ProductCatalog.java
+++ src/testmart/ProductCatalog.java
@@ -1,5 +1,23 @@
 package testmart;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class ProductCatalog {
+	
+	//optional annotation - all public methods in a @WebService are web methods
+	@WebMethod   
+	public List<String> getProductCategories(){
+		
+		List <String> categories=new ArrayList<>();
+		categories.add("Books");
+		categories.add("Movies");
+		categories.add("Music");
+		return categories;
+	}
 
 }
